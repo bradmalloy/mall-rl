078ff0d9659b85e46bcbd85889c9115a7a491b00
Brad Malloy <brad@malloy.io>
2021-01-29 19:35:32 -0800

    first refactor - moved out Enemy. using default exports and bracketed imports

---
+++ js/entities/enemy.js
@@ -0,0 +1,41 @@
+import { Game } from '../main.js';
+
+var Enemy = function(x, y) {
+    this._x = x;
+    this._y = y;
+    this._draw();
+}
+
+Enemy.prototype._draw = function() {
+    Game.display.draw(this._x, this._y, arundelConfig.tiles.enemy, arundelConfig.colors.enemy);
+}
+
+Enemy.prototype.act = function() {
+    var x = Game.player.getX();
+    var y = Game.player.getY();
+    var passableCallback = function(x, y) {
+        return (`${x},${y}` in Game.map);
+    }
+    var astar = new ROT.Path.AStar(x, y, passableCallback, {topology: 4});
+
+    var path = [];
+    var pathCallback = function(x, y) {
+        path.push([x, y]);
+    }
+    astar.compute(this._x, this._y, pathCallback);
+    // Path computation complete
+    path.shift(); // remove the current position
+    if (path.length == 1) {
+        // Attack the player
+        alert("BAM! POW!")
+    } else {
+        x = path[0][0];
+        y = path[0][1];
+        Game.display.draw(this._x, this._y, Game.map[`${this._x},${this._y}`]);
+        this._x = x;
+        this._y = y;
+        this._draw();
+    }
+}
+
+export default Enemy;
